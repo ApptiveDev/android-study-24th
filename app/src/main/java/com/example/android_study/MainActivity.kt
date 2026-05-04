f4131dc5adf8a4f1e5d5ed9b55f8607dbc50bd8b
shinyena00 <syn77@pusan.ac.kr>
2026-05-04 20:34:42 +0900

    네비게이션 바 구현

--- app/src/main/java/com/example/android_study/MainActivity.kt
+++ app/src/main/java/com/example/android_study/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.NavigationRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -35,7 +36,17 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.draw.drawWithContent
@@ -47,20 +58,17 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.layout.PaddingValues
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
-            AndroidstudyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MainScreen()
         }
     }
 }
@@ -72,8 +80,49 @@ data class Post(
     var mainImage : Int,
     var subImages : List<Int>
 )
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun MainScreen(){
+    var selectedTab by remember { mutableStateOf(0) }
+
+    Scaffold(
+        bottomBar = {
+            NavigationBar {
+                NavigationBarItem(
+                    selected = selectedTab == 0,
+                    onClick = {selectedTab = 0},
+                    icon =  {Icon(Icons.Default.Home, contentDescription = null)},
+                    label = {Text("home")}
+                )
+                NavigationBarItem(
+                    selected = selectedTab == 1,
+                    onClick = {selectedTab = 1},
+                    icon = {Icon(Icons.Default.Favorite, null)},
+                    label = {Text("Friend")}
+                )
+                NavigationBarItem(
+                    selected = selectedTab == 2,
+                    onClick = {selectedTab = 2},
+                    icon = {Icon(Icons.Default.Person, null)},
+                    label = {Text("My")}
+
+                )
+            }
+        }
+    ){ innerPadding ->
+        Box(modifier =  Modifier.padding(innerPadding)){
+            when(selectedTab){
+                0 -> HomeScreen(onNavigateToPopular = {selectedTab = 1})
+                1-> Popular()
+                2 -> My()
+            }
+        }
+    }
+}
+
+
+@Composable
+fun HomeScreen(onNavigateToPopular:() -> Unit) {
     val dummy = listOf(
         Post(1, "player1", "bed", R.drawable.bed, listOf(R.drawable.player1, R.drawable.sub1,R.drawable.sub2, R.drawable.sub3, R.drawable.sub4)),
         Post(1, "player2", "clover", R.drawable.clover, listOf(R.drawable.player2, R.drawable.sub1,R.drawable.sub2, R.drawable.sub3, R.drawable.sub4)),
@@ -82,29 +131,31 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
     )
     var selected by remember { mutableStateOf<Post?>(null) }
-    if(selected == null){
+
+    if(selected == null) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .background(Color.Black)
                 .verticalScroll(rememberScrollState())
-        ){
+        ) {
             //배너
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(300.dp),
                 contentAlignment = Alignment.Center
-            ){
+            ) {
                 Image(
                     painter = painterResource(id = R.drawable.apple),
                     contentDescription = "Banner",
                     modifier = Modifier
                         .fillMaxSize()
                         .graphicsLayer {
-                            compositingStrategy = androidx.compose.ui.graphics.CompositingStrategy.Offscreen
+                            compositingStrategy =
+                                androidx.compose.ui.graphics.CompositingStrategy.Offscreen
                         }
-                        .drawWithContent{
+                        .drawWithContent {
                             val gradient = Brush.verticalGradient(
                                 colors = listOf(
                                     Color.Black,
@@ -123,7 +174,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 )
                 Column(
                     horizontalAlignment = Alignment.CenterHorizontally
-                ){
+                ) {
                     Text(text = "Find your own taste", color = Color.White)
                 }
             }
@@ -135,9 +186,14 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                     .padding(horizontal = 16.dp),
                 horizontalArrangement = Arrangement.SpaceBetween,
                 verticalAlignment = Alignment.CenterVertically
-            ){
-                Text( "Popular", color = Color.White)
-                Text("see All >", color = Color.White)
+            ) {
+                Text("Popular", color = Color.White)
+                Text("see Friend >",
+                    color = Color.White,
+                    modifier = Modifier.clickable{
+                        onNavigateToPopular()
+                    }
+                )
             }
             Spacer(modifier = Modifier.height(16.dp))
             //두 줄로 게시물 보여주기
@@ -146,14 +202,14 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                     .fillMaxWidth()
                     .padding(horizontal = 16.dp),
                 horizontalArrangement = Arrangement.spacedBy(16.dp)
-            ){
+            ) {
                 Column(
                     modifier = Modifier.weight(1f),
-                ){
-                    dummy.filterIndexed { index,  _ -> index % 2 == 1}.forEach {post ->
-                        Column (
+                ) {
+                    dummy.filterIndexed { index, _ -> index % 2 == 1 }.forEach { post ->
+                        Column(
                             modifier = Modifier.clickable { selected = post }
-                        ){
+                        ) {
                             Spacer(modifier = Modifier.height(5.dp))
                             Row(
                                 modifier = Modifier
@@ -161,7 +217,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                                     .padding(horizontal = 1.dp),
                                 horizontalArrangement = Arrangement.Start,
                                 verticalAlignment = Alignment.CenterVertically
-                            ){
+                            ) {
                                 Text(post.title, color = Color.Gray)
                             }
                             Image(
@@ -179,7 +235,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                                     .padding(horizontal = 1.dp),
                                 horizontalArrangement = Arrangement.End,
                                 verticalAlignment = Alignment.CenterVertically
-                            ){
+                            ) {
                                 Text(post.userName, color = Color.DarkGray)
                             }
                         }
@@ -188,12 +244,12 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 Column(
                     modifier = Modifier.weight(1f),
                     verticalArrangement = Arrangement.spacedBy((16.dp))
-                ){
+                ) {
 
-                    dummy.filterIndexed { index,  _ -> index % 2 == 0}.forEach {post ->
-                        Column (
+                    dummy.filterIndexed { index, _ -> index % 2 == 0 }.forEach { post ->
+                        Column(
                             modifier = Modifier.clickable { selected = post }
-                        ){
+                        ) {
                             Spacer(modifier = Modifier.height(5.dp))
                             Row(
                                 modifier = Modifier
@@ -201,7 +257,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                                     .padding(horizontal = 1.dp),
                                 horizontalArrangement = Arrangement.Start,
                                 verticalAlignment = Alignment.CenterVertically
-                            ){
+                            ) {
                                 Text(post.title, color = Color.Gray)
                             }
                             Image(
@@ -219,7 +275,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                                     .padding(horizontal = 1.dp),
                                 horizontalArrangement = Arrangement.End,
                                 verticalAlignment = Alignment.CenterVertically
-                            ){
+                            ) {
                                 Text(post.userName, color = Color.DarkGray)
                             }
                         }
@@ -229,81 +285,108 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
         }
     }
-    //계정 상세
     else{
-        val post = selected!!
+        DetailScreen(
+            post = selected!!,
+            onBack = { selected = null }
+        )
+    }
+
+}
+
+@Composable
+fun Popular(){
+    val dummy = listOf(
+        Post(1, "player1", "bed", R.drawable.bed, listOf(R.drawable.player1, R.drawable.sub1,R.drawable.sub2, R.drawable.sub3, R.drawable.sub4)),
+        Post(1, "player2", "clover", R.drawable.clover, listOf(R.drawable.player2, R.drawable.sub1,R.drawable.sub2, R.drawable.sub3, R.drawable.sub4)),
+        Post(1, "player3", "flower", R.drawable.flower, listOf(R.drawable.player3, R.drawable.sub1,R.drawable.sub2, R.drawable.sub3, R.drawable.sub4)),
+        Post(1, "player4", "sea", R.drawable.sea, listOf(R.drawable.player4, R.drawable.sub1,R.drawable.sub2, R.drawable.sub3, R.drawable.sub4))
+
+    )
+    var selected by remember { mutableStateOf<Post?>(null) }
+    Box(modifier = Modifier){
+        Text("확인")
+    }
+}
+@Composable
+fun DetailScreen(post : Post, onBack: () -> Unit){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+    ){
+        Image(
+            painter = painterResource(id = post.subImages[0]),
+            contentDescription = "Background",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .background(Color.Black)
+                .background((Color.Black.copy(alpha = 0.4f)))
+        )
+        IconButton(
+            onClick = onBack,
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(16.dp)
         ){
-            Image(
-                painter = painterResource(id = post.subImages[0]),
-                contentDescription = "Background",
-                modifier = Modifier.fillMaxSize(),
-                contentScale = ContentScale.Crop
-            )
-
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background((Color.Black.copy(alpha = 0.4f)))
-            )
-            IconButton(
-                onClick = { selected = null },
-                modifier = Modifier
-                    .align(Alignment.TopEnd)
-                    .padding(16.dp)
-            ){
-                Text("X", color = Color.White, fontSize = 24.sp)
-            }
-
-            Column (
-                modifier = Modifier
-                    .align(Alignment.CenterStart)
-                    .padding(start = 16.dp)
-                    .width(140.dp)
-                    .fillMaxHeight()
-                    .verticalScroll(rememberScrollState()),
-                verticalArrangement = Arrangement.spacedBy(16.dp)
-            ){
-                Spacer(modifier = Modifier.height(40.dp))
-
-                //사진들 옆에 나오는 거
-                post.subImages.drop(1).forEach { imageRes ->
-                    Image(
-                        painter = painterResource(id = imageRes),
-                        contentDescription = "sub image",
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(180.dp)
-                            .clip(RoundedCornerShape(16.dp))
-                            .border(width = 2.dp, color = Color.Black, shape = RoundedCornerShape(12.dp)),
-                        contentScale = ContentScale.Crop
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                }
-            }
-
-            Text(
-                text = "@${post.userName}",
-                color = Color.White,
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier
-                    .align(Alignment.BottomEnd)
-                    .padding(end = 24.dp, bottom = 24.dp)
-            )
+            Text("X", color = Color.White, fontSize = 24.sp)
         }
+
+        Column (
+            modifier = Modifier
+                .align(Alignment.CenterStart)
+                .padding(start = 16.dp)
+                .width(140.dp)
+                .fillMaxHeight()
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ){
+            Spacer(modifier = Modifier.height(40.dp))
+
+            //사진들 옆에 나오는 거
+            post.subImages.drop(1).forEach { imageRes ->
+                Image(
+                    painter = painterResource(id = imageRes),
+                    contentDescription = "sub image",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(180.dp)
+                        .clip(RoundedCornerShape(16.dp))
+                        .border(width = 2.dp, color = Color.Black, shape = RoundedCornerShape(12.dp)),
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+        }
+
+        Text(
+            text = "@${post.userName}",
+            color = Color.White,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(end = 24.dp, bottom = 24.dp)
+        )
     }
+}
 
 
+@Composable
+fun My(){
+    Box(modifier = Modifier){
+        Text("확인")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AndroidstudyTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
