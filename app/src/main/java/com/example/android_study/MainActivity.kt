67874e4939d0510eb846707ed4942928fc77c033
Abdelrahman Elghandour <abdelrahman.elghandour912@gmail.com>
2026-05-11 18:51:30 +0900

    Added the search Screen and used AI to help with styling the bar:)

--- app/src/main/java/com/example/android_study/MainActivity.kt
+++ app/src/main/java/com/example/android_study/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -38,8 +39,12 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -51,6 +56,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.runtime.setValue
 
 
 class MainActivity : ComponentActivity() {
@@ -60,31 +66,62 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MaterialTheme {
-                MainScreen()
+                App()
             }
         }
     }}
 
+@Composable
+fun App() {
+
+    var currentScreen by remember {
+        mutableStateOf("home")
+    }
+
+    when (currentScreen) {
+
+        "home" -> MainScreen(
+            // The lambda is created inside app so it has access to the variables inside app
+            // This is called closure in programming
+            onScreenChange = {
+                currentScreen = it
+            }
+        )
+
+        "search" -> SearchScreen(
+            onScreenChange = {
+                currentScreen = it
+            }
+        )
+    }
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainScreen() {
+fun MainScreen(
+    onScreenChange: (String) -> Unit
+) {
     val posts = remember { fakePosts() }
+    val stories = remember { fakeStories() }
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
-        topBar = {AppTopBar()},
-        bottomBar = {AppBottomBar()}
-    ) {
+        topBar = { AppTopBar() },
+        bottomBar = {
+            AppBottomBar(
+                onScreenChange = onScreenChange
+            )
+        }    ) {
         paddingValues ->
            LazyColumn(
                modifier = Modifier.fillMaxSize().padding(paddingValues)
            ){
                item {
-                   Row(
+                   LazyRow( // Horizontal Scroll
                    horizontalArrangement = Arrangement.Start
                ) {
-                   fakeStories().forEach {
-                       StoryRing(it)
+                   items(stories) {
+                       story -> StoryRing(story)
                    }
                }}
                items(posts) {
@@ -113,23 +150,51 @@ fun AppTopBar() {
     )
 }
 
+
 @Composable
-fun AppBottomBar() {
+fun AppBottomBar(
+    onScreenChange: (String) -> Unit
+) {
+
     BottomAppBar(
         modifier = Modifier.navigationBarsPadding()
     ) {
+
         Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceEvenly
         ) {
-            BottomBarIcon(Icons.Default.Home, "Home")
-            BottomBarIcon(Icons.Default.Search, "Search")
-            BottomBarIcon(Icons.Default.PlayArrow, "Loops")
-            BottomBarIcon(Icons.Default.Person, "Profile")
+
+            BottomBarIcon(
+                Icons.Default.Home,
+                "Home",
+                onClick = {
+                    onScreenChange("home")
+                }
+            )
+
+            BottomBarIcon(
+                Icons.Default.Search,
+                "Search",
+                onClick = {
+                    onScreenChange("search")
+                }
+            )
+
+            BottomBarIcon(
+                Icons.Default.PlayArrow,
+                "Loops",
+                onClick = {}
+            )
+
+            BottomBarIcon(
+                Icons.Default.Person,
+                "Profile",
+                onClick = {}
+            )
         }
     }
 }
-
 @Composable
 fun TopBarIcon(icon: ImageVector, desc: String) {
     IconButton(onClick = { }) {
@@ -138,8 +203,8 @@ fun TopBarIcon(icon: ImageVector, desc: String) {
 }
 
 @Composable
-fun BottomBarIcon(icon: ImageVector, desc: String) {
-    IconButton(onClick = {}) {
+fun BottomBarIcon(icon: ImageVector, desc: String, onClick: () -> Unit) {
+    IconButton(onClick =  onClick) {
         Icon(icon, contentDescription = desc)
     }
 }
@@ -222,6 +287,67 @@ fun StoryRing(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchScreen(
+    onScreenChange: (String) -> Unit
+) {
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+
+        bottomBar = {
+            AppBottomBar(
+                onScreenChange = onScreenChange
+            )
+        }
+
+    ) { paddingValues ->
+
+        SearchBar(
+            modifier = Modifier.padding(paddingValues)
+        )
+    }
+}
+
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier
+) {
+    var name by remember { mutableStateOf("") }
+    TextField(
+        modifier = modifier.fillMaxWidth().padding(16.dp),
+        value = name,
+        onValueChange = { name = it },
+        label = { Text("Search") },
+        placeholder = {
+            Text("Search")
+        },
+
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = "Search"
+            )
+        },
+
+        singleLine = true,
+
+        shape = RoundedCornerShape(24.dp),
+
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = Color(0xFFF2F2F2),
+            unfocusedContainerColor = Color(0xFFF2F2F2),
+
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        )
+
+    )
+}
+
+
+
 fun fakePosts() = listOf(
     PostData(R.drawable.post_2, "Switzerland ❤️"),
     PostData(R.drawable.post_3, "Football ❤️")
@@ -231,6 +357,12 @@ fun fakeStories(): List<Int> = listOf(
     R.drawable.story1,
     R.drawable.story2,
     R.drawable.story3,
+    R.drawable.story3,
+    R.drawable.story3,
+    R.drawable.story3,
+    R.drawable.story3,
+    R.drawable.story3,
+
 )
 data class PostData (
     @DrawableRes val imageRes: Int,
